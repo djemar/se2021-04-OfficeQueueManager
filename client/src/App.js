83c1a47162d9d59acbf5717840b24d4d7c05623f
MOVdario <79694881+MOVdario@users.noreply.github.com>
2021-10-15 22:55:53 +0200

    Task #15 - "Ticket numbers are unique for the whole             office" fixing

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import QueueSet from './QueueSet.js';
 import { useState } from 'react';
+import { Container } from 'react-bootstrap';
 
 function App() {
   let [counter, setCounter] = useState(2);
+  let [user, setUser] = useState(0);
+  let [userTicket, setUserTicket] = useState(-1);
+  let [loadingTicket, setLoadingTicket] = useState(true);
+  let [mainHead, setMainHead] = useState(0);
+  let [mainTail, setMainTail] = useState(0);
+
+  let pairings = []; // value in position i means that the i-th ticket is associated to the indicated service
 
   return (
     <div className="App">
       <strong>Queues branch</strong>
-      <h1>Counter: {counter}</h1>
-      <QueueSet counter={counter} user={0} />
+      {!user ? (
+        <>
+          <h1>Counter: {counter}</h1>
+          <div>
+            {userTicket !== -1 ? (
+              !loadingTicket ? (
+                <Container fluid>
+                  <h1>
+                    Your ticket number is: <strong>{userTicket}</strong>
+                  </h1>
+                </Container>
+              ) : (
+                <div />
+              )
+            ) : (
+              <div />
+            )}
+          </div>
+        </>
+      ) : (
+        <div />
+      )}
+      <QueueSet
+        counter={counter}
+        user={user}
+        userTicket={userTicket}
+        setUserTicket={setUserTicket}
+        setLoadingTicket={setLoadingTicket}
+        mainHead={mainHead}
+        setMainHead={setMainHead}
+        mainTail={mainTail}
+        setMainTail={setMainTail}
+        pairings={pairings}
+      />
     </div>
   );
 }
--- client/src/QueueSet.js
+++ client/src/QueueSet.js
@@ -5,12 +5,28 @@ function QueueSet(props) {
   /* This function is used to understand which queues are needed for this specific counter */
   //let [services, setServices] = useState([]);
 
+  const allServices = [1, 2, 3, 4, 5, 6];
   /* todo: fetch to the DB in order to get the services of this props.counter */
-  let tmp_services = [1, 2];
+  let currentServices = [1, 2];
   //setServices(tmp_services);
 
-  return tmp_services.map(index => (
-    <Queue index={index} counter={props.counter} user={props.user} />
+  if (!props.user) {
+    currentServices = allServices; // user can see all the queues
+  }
+  return currentServices.map(index => (
+    <Queue
+      index={index}
+      counter={props.counter}
+      user={props.user}
+      userTicket={props.userTicket}
+      setUserTicket={props.setUserTicket}
+      setLoadingTicket={props.setLoadingTicket}
+      pairings={props.pairings}
+      mainHead={props.mainHead}
+      setMainHead={props.setMainHead}
+      mainTail={props.mainTail}
+      setMainTail={props.setMainTail}
+    />
   ));
 }
 
@@ -25,19 +41,27 @@ function Queue(props) {
       {props.user === 0 ? (
         <Container>
           <Row className="justify-content-md-center">
-            User button: displays last ticket
+            User button: displays number of people waiting on this service
           </Row>
           <Row>
             <Col />
             <Col>
-              <Button
-                onClick={event => {
-                  event.preventDefault();
-                  setTail(tail + 1);
-                }}
-              >
-                {!tail ? 'EMPTY' : tail}
-              </Button>
+              {props.userTicket === -1 ? (
+                <Button
+                  onClick={event => {
+                    event.preventDefault();
+                    props.setUserTicket(props.mainTail + 1);
+                    setTail(tail + 1);
+                    props.pairings.push(props.index);
+                    props.setMainTail(props.mainTail + 1);
+                    props.setLoadingTicket(false);
+                  }}
+                >
+                  {!tail ? 'EMPTY' : tail}
+                </Button>
+              ) : (
+                <Button>{tail}</Button>
+              )}
             </Col>
             <Col />
           </Row>
@@ -53,7 +77,10 @@ function Queue(props) {
               <Button
                 onClick={event => {
                   event.preventDefault();
-                  if (tail >= 1 && head < tail) setHead(head + 1);
+                  if (tail >= 1 && head < tail) {
+                    props.setMainHead(head + 1);
+                    setHead(head + 1);
+                  }
                 }}
               >
                 {!tail ? 'EMPTY' : head}
