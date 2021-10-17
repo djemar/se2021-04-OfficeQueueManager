b345ce962878463b2591c6d0110f445e4ab7d6f9
Diego Marino <marinodiego.96@gmail.com>
2021-10-17 09:44:29 +0200

    add Login btn render test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,6 +7,7 @@ describe('App', () => {
   test('renders App component', () => {
     render(<App />);
 
+    expect(screen.getByText(/Login/)).toBeInTheDocument(); //check to see if login btn is rendered
     screen.debug();
   });
 });
